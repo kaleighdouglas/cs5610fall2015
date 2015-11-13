54a3e22d9311a00dd57c2ffb98a4c0a94e31314d
Kaleigh OHara <kohara@ccs.neu.edu>
2015-11-13 14:12:06 -0500

    rearranged lines in server

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
-var upload = multer(); // for parsing multipart/form-data
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-//app.use(multer()); // for parsing multipart/form-data
+//var upload = multer(); // for parsing multipart/form-data
+
+
+
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
 
