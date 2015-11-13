78bd8b570bd6591b2c2329d2b576d85cc7edf805
Kaleigh OHara <kohara@ccs.neu.edu>
2015-11-13 12:59:57 -0500

    minor server edit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
-//var upload = multer(); // for parsing multipart/form-data
+var upload = multer(); // for parsing multipart/form-data
 
 
 app.use(express.static(__dirname + '/public'));
@@ -12,7 +12,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(multer()); // for parsing multipart/form-data
+//app.use(multer()); // for parsing multipart/form-data
 
 require("./public/assignment/server/app.js")(app);
 
