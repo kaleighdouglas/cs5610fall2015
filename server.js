0f4f3b29d5fe5971a47f5b68f2482504456b9289
Kaleigh OHara <kohara@ccs.neu.edu>
2015-11-13 15:28:22 -0500

    added bodyparser and multer to package.json

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
-
-//var bodyParser = require('body-parser');
-//var multer = require('multer'); // v1.0.5
-//var upload = multer(); // for parsing multipart/form-data
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+//var upload = multer(); // for parsing multipart/form-data
+
 
 
 app.use(express.static(__dirname + '/public'));
 
-//app.use(bodyParser.json()); // for parsing application/json
-//app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-//app.use(multer()); // for parsing multipart/form-data
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
