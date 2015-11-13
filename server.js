5faae58824c98612e04cb0a3058935297c40ecae
Kaleigh OHara <kohara@ccs.neu.edu>
2015-11-13 14:21:50 -0500

    commented out bodyparser and multer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
-var bodyParser = require('body-parser');
-var multer = require('multer'); // v1.0.5
+//var bodyParser = require('body-parser');
+//var multer = require('multer'); // v1.0.5
 //var upload = multer(); // for parsing multipart/form-data
 var app = express();
 
