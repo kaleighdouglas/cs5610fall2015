299938d68cf4dd25bc85019bdbf98b8ef39ec569
Kaleigh OHara <kohara@ccs.neu.edu>
2015-11-13 14:19:09 -0500

    rearranged lines in server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
-var app = express();
+
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 //var upload = multer(); // for parsing multipart/form-data
+var app = express();
+
 
 app.use(express.static(__dirname + '/public'));
 
