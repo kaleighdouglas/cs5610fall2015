1f6ee4e725c983b17c53e25fee91525ceeb2af54
Kaleigh OHara <kohara@ccs.neu.edu>
2015-11-18 22:35:48 -0500

    updated file structure

--- public/project/client/config.js
+++ public/project/client/config.js
@@ -7,58 +7,58 @@
     function Configure($routeProvider) {
         $routeProvider
              .when("/home",{
-                templateUrl: "home/home.view.html"
+                templateUrl: "views/home/home.view.html"
             })
             .when("/login", {
-                templateUrl: "login/login.view.html",
+                templateUrl: "views/login/login.view.html",
                 controller: "LoginController"
             }) 
             .when("/register", {
-                templateUrl: "register/register.view.html",
+                templateUrl: "views/register/register.view.html",
                 controller: "RegisterController"
             })
             .when("/profile", {
-                templateUrl: "profile/profile.view.html",
+                templateUrl: "views/profile/profile.view.html",
                 controller: "ProfileController"
             })
             .when("/history", {
-                templateUrl: "history/history.view.html",
+                templateUrl: "views/history/history.view.html",
                 controller: "HistoryController"
             })
             .when("/question", {
-                templateUrl: "question/question.view.html",
+                templateUrl: "views/question/question.view.html",
                 controller: "QuestionController"
             })
             .when("/methodProCon", {
-                templateUrl: "methodProCon/methodProCon.view.html",
+                templateUrl: "views/methodProCon/methodProCon.view.html",
                 controller: "MethodProConController"
             })
             .when("/methodIntuitionOptions", {
-                templateUrl: "methodIntuition/methodIntuitionOptions.view.html",
+                templateUrl: "views/methodIntuition/methodIntuitionOptions.view.html",
                 controller: "MethodIntuitionOptionsController"
             })
             .when("/methodIntuitionEval", {
-                templateUrl: "methodIntuition/methodIntuitionEval.view.html",
+                templateUrl: "views/methodIntuition/methodIntuitionEval.view.html",
                 controller: "MethodIntuitionEvalController"
             })
             .when("/methodGridOptions", {
-                templateUrl: "methodGrid/methodGridOptions.view.html",
+                templateUrl: "views/methodGrid/methodGridOptions.view.html",
                 controller: "MethodGridOptionsController"
             })
             .when("/methodGridAttributes", {
-                templateUrl: "methodGrid/methodGridAttributes.view.html",
+                templateUrl: "views/methodGrid/methodGridAttributes.view.html",
                 controller: "MethodGridAttributesController"
             })
             .when("/methodGridEval", {
-                templateUrl: "methodGrid/methodGridEval.view.html",
+                templateUrl: "views/methodGrid/methodGridEval.view.html",
                 controller: "MethodGridEvalController"
             })
             .when("/advice", {
-                templateUrl: "advice/advice.view.html",
+                templateUrl: "views/advice/advice.view.html",
                 controller: "AdviceController"
             })
             .when("/decision", {
-                templateUrl: "decision/decision.view.html",
+                templateUrl: "views/decision/decision.view.html",
                 controller: "DecisionController"
             })
             .otherwise({
