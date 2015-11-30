763a85f1fe46b7702e1812fd9d560f65753b4740
Kaleigh OHara <kohara@ccs.neu.edu>
2015-11-30 18:29:01 -0500

    server update with new db connection

--- server.js
+++ server.js
@@ -4,7 +4,22 @@ var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 //var upload = multer(); // for parsing multipart/form-data
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/cs5610');
+
+
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610'; //-oharakaleigh
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+
+
+//var db = mongoose.connect('mongodb://localhost/cs5610');
 //console.log(mongoose);
 
 
@@ -15,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.use(multer()); // for parsing multipart/form-data
 
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP; // || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
 
 
